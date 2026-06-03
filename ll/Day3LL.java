1942dd55c00a633109e0a28e51456fd9f66cb548
KANDUKURI ABHINAV <abhinavkandukuri05@gmail.com>
2026-06-03 11:54:19 +0530

    Update Day3LL.java

--- ll/Day3LL.java
+++ ll/Day3LL.java
@@ -1,7 +1,38 @@
-
 import java.util.*;
 
 class Day3LL {
+
+    public static boolean checkDuplicatesM1(int n, int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+
+            for (int j = i + 1; j < arr.length; j++) {
+
+                if (arr[i] == arr[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean checkDuplicatesM2(int n, int[] arr) {
+
+        Set<Integer> hashSet = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (hashSet.contains(arr[i])) {
+                return true;
+            } 
+            else {
+                hashSet.add(arr[i]);
+            }
+        }
+
+        return false;
+    }
 
     public static void main(String[] args) {
 
@@ -13,6 +44,7 @@ class Day3LL {
 
         System.out.println("Stack: " + stack + " -> LIFO Order");
 
+
         LinkedList<Integer> linkedList = new LinkedList<>();
 
         linkedList.add(10);
@@ -20,6 +52,7 @@ class Day3LL {
         linkedList.add(30);
 
         System.out.println("LinkedList: " + linkedList + " -> Insertion Order");
+
 
         ArrayList<Integer> arrayList = new ArrayList<>();
 
@@ -29,6 +62,7 @@ class Day3LL {
 
         System.out.println("ArrayList: " + arrayList + " -> Insertion Order");
 
+
         HashSet<Integer> hashSet = new HashSet<>();
 
         hashSet.add(30);
@@ -36,6 +70,7 @@ class Day3LL {
         hashSet.add(20);
 
         System.out.println("HashSet: " + hashSet + " -> Unsorted");
+
 
         TreeSet<Integer> treeSet = new TreeSet<>();
 
@@ -45,6 +80,7 @@ class Day3LL {
 
         System.out.println("TreeSet: " + treeSet + " -> Sorted");
 
+
         LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
 
         linkedHashSet.add(30);
@@ -52,5 +88,30 @@ class Day3LL {
         linkedHashSet.add(20);
 
         System.out.println("LinkedHashSet: " + linkedHashSet + " -> Insertion Order");
+
+
+        // Return true if value appears twice else false
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements: ");
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean res = checkDuplicatesM1(n, arr);
+
+        System.out.println("Method1: " + res);
+
+        boolean rese = checkDuplicatesM2(n, arr);
+
+        System.out.println("Method2: " + rese);
     }
 }
