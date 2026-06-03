cbde3b2c33ffa4fa1273e338ced646282f7035c0
KANDUKURI ABHINAV <abhinavkandukuri05@gmail.com>
2026-06-03 10:51:59 +0530

    Add files via upload

---
+++ ll/Day3LL.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println(stack);
+
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(10);
+        linkedList.add(20);
+        linkedList.add(30);
+
+        System.out.println(linkedList);
+
+        ArrayList<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+
+        System.out.println(arrayList);
+    }
+}
