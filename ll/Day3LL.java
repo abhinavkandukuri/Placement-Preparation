a9a86fdfe1b6fd66565a35bcce31301f91126879
KANDUKURI ABHINAV <abhinavkandukuri05@gmail.com>
2026-06-03 11:33:00 +0530

    Update Day3LL.java

--- ll/Day3LL.java
+++ ll/Day3LL.java
@@ -1,7 +1,7 @@
 
 import java.util.*;
 
-class Main {
+class Day3LL {
 
     public static void main(String[] args) {
 
@@ -11,15 +11,15 @@ class Main {
         stack.push(20);
         stack.push(30);
 
-        System.out.println(stack);
+        System.out.println("Stack: " + stack + " -> LIFO Order");
 
         LinkedList<Integer> linkedList = new LinkedList<>();
-  
+
         linkedList.add(10);
         linkedList.add(20);
         linkedList.add(30);
 
-        System.out.println(linkedList);
+        System.out.println("LinkedList: " + linkedList + " -> Insertion Order");
 
         ArrayList<Integer> arrayList = new ArrayList<>();
 
@@ -27,6 +27,30 @@ class Main {
         arrayList.add(20);
         arrayList.add(30);
 
-        System.out.println(arrayList);
+        System.out.println("ArrayList: " + arrayList + " -> Insertion Order");
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        hashSet.add(30);
+        hashSet.add(10);
+        hashSet.add(20);
+
+        System.out.println("HashSet: " + hashSet + " -> Unsorted");
+
+        TreeSet<Integer> treeSet = new TreeSet<>();
+
+        treeSet.add(30);
+        treeSet.add(10);
+        treeSet.add(20);
+
+        System.out.println("TreeSet: " + treeSet + " -> Sorted");
+
+        LinkedHashSet<Integer> linkedHashSet = new LinkedHashSet<>();
+
+        linkedHashSet.add(30);
+        linkedHashSet.add(10);
+        linkedHashSet.add(20);
+
+        System.out.println("LinkedHashSet: " + linkedHashSet + " -> Insertion Order");
     }
 }
